dc5556f92d58009fc9e2e8ffa85a8444941b812a
Lucas Vido <vidov8@gmail.com>
2025-08-29 12:56:47 -0300

    Version bump

--- elasticutils/_version.py
+++ elasticutils/_version.py
@@ -3,5 +3,5 @@
 # * 0.3     - released version
 # * 0.3a1   - alpha version
 # * 0.3.dev - version in development
-__version__ = '0.10.3'
-__releasedate__ = '20150304'
+__version__ = '0.10.5'
+__releasedate__ = '20250829'
